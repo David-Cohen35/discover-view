194b3748a6071c2e9b1a576fd537ec1694ffb8e2
David Cohen <dovidcohen35@gmail.com>
2023-01-23 12:36:27 -0500

    commenting out everything to resubmit pr

--- frontend/src/components/DiscoverView.js
+++ frontend/src/components/DiscoverView.js
@@ -1,38 +1,38 @@
-import React, { useState } from "react";
-import DateRangePicker from "./DateRangePicker";
-import Graph from "./Graph";
-import Table from "./Table";
+// import React, { useState } from "react";
+// import DateRangePicker from "./DateRangePicker";
+// import Graph from "./Graph";
+// import Table from "./Table";
 
-const DiscoverView = () => {
-	const [, setStartDate] = useState(null);
-	const [, setEndDate] = useState(null);
-	const [data, setData] = useState([]);
-	const [error, setError] = useState(null);
+// const DiscoverView = () => {
+// 	const [, setStartDate] = useState(null);
+// 	const [, setEndDate] = useState(null);
+// 	const [data, setData] = useState([]);
+// 	const [error, setError] = useState(null);
 
-	return (
-		<>
-			{error === null ? (
-				<div className='flex justify-center flex-col'>
-					<DateRangePicker
-						setStartDate={setStartDate}
-						setEndDate={setEndDate}
-						setData={setData}
-						setError={setError}
-					/>
-					<div className='border border-solid border-gray-500 m-2.5 p-1 rounded'>
-						<div className='flex justify-center'>
-							<Graph data={data} />
-						</div>
-						<Table data={data} />
-					</div>
-				</div>
-			) : (
-				<h2>
-					Uh oh! There was an error fetching your data! Please try again later
-				</h2>
-			)}
-		</>
-	);
-};
+// 	return (
+// 		<>
+// 			{error === null ? (
+// 				<div className='flex justify-center flex-col'>
+// 					<DateRangePicker
+// 						setStartDate={setStartDate}
+// 						setEndDate={setEndDate}
+// 						setData={setData}
+// 						setError={setError}
+// 					/>
+// 					<div className='border border-solid border-gray-500 m-2.5 p-1 rounded'>
+// 						<div className='flex justify-center'>
+// 							<Graph data={data} />
+// 						</div>
+// 						<Table data={data} />
+// 					</div>
+// 				</div>
+// 			) : (
+// 				<h2>
+// 					Uh oh! There was an error fetching your data! Please try again later
+// 				</h2>
+// 			)}
+// 		</>
+// 	);
+// };
 
-export default DiscoverView;
+// export default DiscoverView;
